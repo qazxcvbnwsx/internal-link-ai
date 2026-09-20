e6c9b8c26899662c0010724a7ce349445947ef80
qazxcvbnwsx <magdalenanafalska@gmail.com>
2026-09-20 17:10:10 +0200

    Update crawler.py

--- internal_linking/crawler.py
+++ internal_linking/crawler.py
@@ -1,1 +1,22 @@
+import requests
 
+
+USER_AGENT = (
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+    "AppleWebKit/537.36 (KHTML, like Gecko) "
+    "Chrome/153.0.0.0 Safari/537.36"
+)
+
+
+def download_page(url):
+    response = requests.get(
+        url,
+        timeout=30,
+        headers={
+            "User-Agent": USER_AGENT
+        }
+    )
+
+    response.raise_for_status()
+
+    return response.text
